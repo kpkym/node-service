769ad803309dbdebc8adcd67da098c4dfad3013b
kpkym <okpkym@gmail.com>
2023-04-19 18:11:25 +0800

    testing motify

--- src/furigana/furigana.test.ts
+++ src/furigana/furigana.test.ts
@@ -1,13 +1,16 @@
 import {expect, test} from '@jest/globals';
 import {exportedForTesting} from './furigana'
-import Log from "../../config/logging";
+import {FuriganaServiceConvertRequest} from "../../gen/furigana/v1/request_pb";
 
 
 test('check', async () => {
     let input = "感じ取れたら手を繋ごう、重なるのは人生のライン and レミリア最高！";
 
-    let output = await exportedForTesting.parse(input);
-    Log.info(output)
+    let furiganaServiceConvertRequest = new FuriganaServiceConvertRequest();
+    furiganaServiceConvertRequest.setBody(input)
+    furiganaServiceConvertRequest.setTo('hiragana')
+    furiganaServiceConvertRequest.setMode('furigana')
+    await exportedForTesting.parse(furiganaServiceConvertRequest);
 });
 
 
