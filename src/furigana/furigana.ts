968aebd6df846355c3953ca3064f101a0e41deb8
kpkym <okpkym@gmail.com>
2023-04-19 11:43:54 +0800

    add kuroshiro

--- src/furigana/furigana.ts
+++ src/furigana/furigana.ts
@@ -2,12 +2,23 @@ import {IFuriganaServiceServer} from 'gen/furigana/v1/request_grpc_pb';
 import * as grpc from '@grpc/grpc-js';
 import {sendUnaryData, UntypedHandleCall} from '@grpc/grpc-js';
 import {FuriganaServiceConvertRequest, FuriganaServiceConvertResponse} from '../../gen/furigana/v1/request_pb';
+import Kuroshiro from "kuroshiro";
+import KuromojiAnalyzer from "kuroshiro-analyzer-kuromoji";
+
 
 export class FuriganaServiceServer implements IFuriganaServiceServer {
-    convert(call: grpc.ServerUnaryCall<FuriganaServiceConvertRequest, FuriganaServiceConvertResponse>, callback: sendUnaryData<FuriganaServiceConvertResponse>): void {
+    async convert(call: grpc.ServerUnaryCall<FuriganaServiceConvertRequest, FuriganaServiceConvertResponse>, callback: sendUnaryData<FuriganaServiceConvertResponse>): Promise<void> {
         console.log(`${new Date().toISOString()}    request ${call.request.getBody()}`);
+
+        const kuroshiro = new Kuroshiro();
+        // Initialize
+        // Here uses async/await, you could also use Promise
+        await kuroshiro.init(new KuromojiAnalyzer());
+        // Convert what you want
+        const result = await kuroshiro.convert(call.request.getBody(), {mode: "furigana", to: "hiragana"});
+
         const furiganaServiceConvertResponse = new FuriganaServiceConvertResponse();
-        furiganaServiceConvertResponse.setBody(`hello: ${call.request.getBody()}`);
+        furiganaServiceConvertResponse.setBody(`hello: ${result}`);
         callback(null, furiganaServiceConvertResponse);
     }
 
